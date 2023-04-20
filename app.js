0186ab1fe275a16570a49ff0be1ebb9d25308038
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-04-20 20:31:08 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+
+// app.use(bodyParser.urlencoded())  // x-www-form-urlencoded <form>
+app.use(bodyParser.json()) // application/json
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE')
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
+
+app.listen(8282, () => {
+    console.log("App started on port 8282")
+}); 
