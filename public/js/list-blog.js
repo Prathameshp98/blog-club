9dda00a3054a66d7f8fd133c01e7f11037ed1fdd
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-06-07 17:43:47 +0530

    like dislike from client side

--- public/js/list-blog.js
+++ public/js/list-blog.js
@@ -1,8 +1,14 @@
 
 const show_more = document.getElementById('show_more')
 const parent = document.getElementById('parent')
+const like = document.getElementById('like')
 
 
-// show_more.addEventListener("click", function(){
+like.addEventListener("click", function(){
+    if(like.getAttribute("src") === "/images/red_heart.webp"){
+        like.setAttribute("src","/images/heart.webp")
+    } else {
+        like.setAttribute("src","/images/red_heart.webp")
+    }
     
-// });
+});
