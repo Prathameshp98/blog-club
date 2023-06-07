f4c5ab46cff882344177e310b0fc9451c2d9ef3b
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-06-07 20:06:17 +0530

    hide element functionality implemented by id

--- public/js/list-blog.js
+++ public/js/list-blog.js
@@ -2,19 +2,40 @@
 const show_more = document.getElementById('show_more')
 const parent = document.getElementById('parent')
 const like = document.querySelectorAll('.like')
+const remove = document.querySelectorAll('.remove')
 
 like.forEach(element => {
     element.addEventListener("click", function(event){
 
         const targetId = event.target.id;
         const targetElement = document.getElementById(targetId)
-        console.log(targetId)
     
         if(targetElement.getAttribute("src") === "/images/red_heart.webp"){
             targetElement.setAttribute("src","/images/heart.webp")
         } else {
             targetElement.setAttribute("src","/images/red_heart.webp")
         }
+        
+    });
+})
+
+remove.forEach(element => {
+    element.addEventListener("click", function(event){
+
+        let targetId = event.target.id;
+        targetId = targetId.replace("_remove","") + "_blog"
+        const targetElement = document.getElementById(targetId);
+        console.log(targetId)
+        console.log(targetElement)
+
+        targetElement.innerHTML = "<p class='blog-removal'>You won't see this blog again</p>"
+
+
+        const hideElement = () => {
+            targetElement.style.display = 'none';
+        };
+
+        setTimeout(hideElement, 2000)
         
     });
 })
