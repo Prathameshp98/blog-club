ac615e382d95ffde7bc44cc9f34d7a4fd56d8d75
Prathamesh Patil <prathamesh.patil@ufaber.com>
2023-06-07 19:35:12 +0530

    like and dislike by id

--- public/js/list-blog.js
+++ public/js/list-blog.js
@@ -1,14 +1,21 @@
 
 const show_more = document.getElementById('show_more')
 const parent = document.getElementById('parent')
-const like = document.getElementById('like')
+const like = document.querySelectorAll('.like')
 
+like.forEach(element => {
+    element.addEventListener("click", function(event){
 
-like.addEventListener("click", function(){
-    if(like.getAttribute("src") === "/images/red_heart.webp"){
-        like.setAttribute("src","/images/heart.webp")
-    } else {
-        like.setAttribute("src","/images/red_heart.webp")
-    }
+        const targetId = event.target.id;
+        const targetElement = document.getElementById(targetId)
+        console.log(targetId)
     
-});
+        if(targetElement.getAttribute("src") === "/images/red_heart.webp"){
+            targetElement.setAttribute("src","/images/heart.webp")
+        } else {
+            targetElement.setAttribute("src","/images/red_heart.webp")
+        }
+        
+    });
+})
+
